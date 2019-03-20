e0fe3ab8e04602b4beade5dabd54240142d07045
Erick Calder <e@arix.com>
2019-03-20 16:06:15 -0700

    restructured tests to handle promises better

--- t/index.js
+++ t/index.js
@@ -2,29 +2,39 @@ const assert = require('assert').strict
 const sms = require('../index.js')
 
 describe('sms-receive', () => {
-    it('Should return multiple numbers', async () => {
-        var actual = await sms.numbers()
-        assert.notEqual(actual.length, 0, 'No numbers found')
+    describe('Numbers', () => {
+        var actual;
+        before(async () => {
+            actual = await sms.numbers();
+        })
+        it('Should return multiple choices', async () => {
+            assert.notEqual(actual.length, 0, 'No numbers found')
+        })
+        it('Records should have number and location', async () => {
+            assert.ok(actual[0].nbr, 'No number returned')
+            assert.ok(actual[0].loc, 'No location returned')
+        })
+        it('Numbers should be properly formatted', async () => {
+            assert.ok(!actual[0].nbr.match(/^\+$/), 'Does not begin with a +')
+            assert.ok(actual[0].nbr.match(/^\+[\d ]+$/), 'Contains unsupported characters')
+        })    
     })
-    it('Records should have number and location', async () => {
-        var actual = await sms.numbers()
-        assert.ok(actual[0].nbr, 'No number returned')
-        assert.ok(actual[0].loc, 'No location returned')
-    })
-    it('Numbers should be properly formatted', async () => {
-        var actual = await sms.numbers()
-        assert.ok(!actual[0].nbr.match(/^\+$/), 'Does not begin with a +')
-        assert.ok(actual[0].nbr.match(/^\+[\d ]+$/), 'Contains unsupported characters')
-    })
-    it('Returns countries', async () => {
-        let ls = await sms.countries();
-        assert.ok(Array.isArray(ls), 'Is not an array')
-        assert.ok(ls.length > 0, 'Array is empty')
-        var found = false;
-        for (var i = 0; i < ls.length; i++)
-            if (ls.indexOf(ls[i]) != i) {
-                found = true; break;
-            }
-        assert.ok(!found, 'Array is distinct')
+    describe('Countries', () => {
+        var ls;
+        before(async () => {
+            ls = await sms.countries()
+        })
+        it('Is non-empty array', () => {
+            assert.ok(Array.isArray(ls), 'Is not an array')
+            assert.ok(ls.length > 0, 'Array is empty')
+        })
+        it('Contains a distinct list', () => {
+            var found = false;
+            for (var i = 0; i < ls.length; i++)
+                if (ls.indexOf(ls[i]) != i) {
+                    found = true; break;
+                }
+            assert.ok(!found, 'Array is not distinct')
+        })
     })
 })
